4dc601b43ea0d4dfca9192389c2b496fddea6332
Sumit <veerkhanduri@gmail.com>
2022-03-08 08:40:25 -0800

    Updated the firbase api

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyCufb_kw0ht6MyUwUSoNpDdACqlGUYFJYk",
+  authDomain: "fir-comp1800-219ce.firebaseapp.com",
+  projectId: "fir-comp1800-219ce",
+  storageBucket: "fir-comp1800-219ce.appspot.com",
+  messagingSenderId: "920526734130",
+  appId: "1:920526734130:web:d92fb6d0e1aa333c010d2b"
 };
 
 //--------------------------------------------
